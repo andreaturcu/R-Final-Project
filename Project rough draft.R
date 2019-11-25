5591e64aa4e7b915a19932d5a56d00f250b74b62
andreaturcu <aleathead@gmail.com>
2019-11-25 15:50:25 -0600

    Used the CSV file

--- Project rough draft.R
+++ Project rough draft.R
@@ -1,8 +1,7 @@
-library("readxl")
-my_data <- read_excel(file.choose())
-mg<-my_data
+mg<-read.csv(file.choose())
 is.data.frame(mg)
 sr<-mg[mg$No. < 2, ]
 sr2<-subset(mg, No.<2)
 ordermg<-order(mg$Time_Bud,mg$Fl_Time, na.last = NA)
 mg_ordered<-mg[ordermg,]
+
