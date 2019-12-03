89546e650448aa229dcfc83a793ddf513ee20645
andreaturcu <aleathead@gmail.com>
2019-12-02 20:05:33 -0600

    Halfway through ggploting

--- Project rough draft.R
+++ Project rough draft.R
@@ -6,25 +6,58 @@ ordermg<-order(mg$Time_Bud,mg$Fl_Time, na.last = NA)
 mg_ordered<-mg[ordermg,]
 library(tidyverse)
 summarize(mg_ordered, avg_st_hgt=mean(St_Height))
+
 #Custom function:
 mm_to_cm<-function(x){
   cm<-x/10
   return(cm)
 }
-mm_to_cm(mg_ordered$St_Diam)
-#Custom operator:
+mg_ordered$St_Diam_cm<-mm_to_cm(mg_ordered$St_Diam)
+
+
+#Custom operator with if/else statement:
 '%less.than%' <- function(x, y) {
   if (x<y) return (TRUE)
   else return (FALSE)
 }
-'%less.than%'(mg_ordered$Fl_Length,mg_ordered$Fl_Wid)
 
+'%less.than%'(mg_ordered$Fl_Length,mg_ordered$Fl_Wid)#Only returns first value
 
+#For loop:
+for(i in 1:nrow(mg_ordered)){
+  print('%less.than%'(mg_ordered$Fl_Length[i],mg_ordered$Fl_Wid[i]))
+}#Returns all values of these columns
 
-
-
-
-#Later?
-#Below: maybe in ddply section:
+#ddply:
 leaves <- tapply(X = mg_ordered$Leaf_No, INDEX = list(mg_ordered$Pop), FUN = sum)
-leaves  
+
+#Histogram:
+hist(mg_ordered$St_Height, 
+     main="Stem Height", 
+     xlab="Height (cm)",
+     ylab="Number of Individuals",
+     border="blue", 
+     col="pink",
+     las=1,
+     breaks=30,
+     xlim=c(0,25))
+
+#Line Plot;
+HOV<-mg_ordered[mg_ordered$Pop=="HOV",]
+plot(x=HOV$Brnc_No, y=HOV$Leaf_No)
+
+#ggplot:
+library(ggplot2)
+ggplot(data= mg_ordered, aes(x=Brnc_No, y=Leaf_No, colour=Pop)) + 
+  geom_point() +
+  scale_color_manual(values=c("purple","pink","black","green","yellow","orange","grey","red","brown","turquoise","white","tan"))+
+  ggtitle("Relationship between Branch and Leaf Numbers by Population")+
+  xlab("Number of Branches")+
+  ylab("Number of Leaves")
+  
+
+
+
+
+
+ 
